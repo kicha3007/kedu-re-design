faf55586254bd42b892a31bd162cd3c1db2dd937
kicha3007 <kicha3007@gmail.com>
2019-01-18 16:09:32 +0500

    dev header menu

--- build/static/js/ui.js
+++ build/static/js/ui.js
@@ -3,7 +3,7 @@ $(document).ready(function()
     /* HEADER SLIDER */
     var $search_bg = $('.search-bg');
 
-/*    if($search_bg.jquery) {
+    /*if($search_bg.jquery) {
         var $i = 0,
             $i_max = 7;
 
@@ -58,7 +58,7 @@ $(document).ready(function()
     });
 
     /* LOADERS */
- /*   BX.showWait = function(node, msg) {
+/*    BX.showWait = function(node, msg) {
        $(node).addClass('action-load');
     };
 
